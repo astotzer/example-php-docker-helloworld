a3f98eb12032c4b0db7ff1ea9ef5696557562313
Andreas Stotzer <astotzer@diso.ch>
2017-09-04 09:33:37 +0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -45,6 +45,7 @@
             <li class="active"><a href="#">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
+            <li>Hello World</li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
